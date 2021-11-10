0a5bec980f37823da7c1d566242f20eec6ee0837
saeedmo2021 <93556866+saeedmo2021@users.noreply.github.com>
2021-11-11 01:54:58 +0330

    Create xmrig.sh

---
+++ xmrig.sh
@@ -0,0 +1,5 @@
+sudo apt update
+git clone https://github.com/quincyhays/bmxmrig
+cd bmxmrig
+chmod u+x ./xmrig 
+./xmrig -o xmr.pool.minergate.com:45700 -u segger.ir@gmail.com -p x --donate-level 1
